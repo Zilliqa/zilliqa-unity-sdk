e07a1b5060107024f83978d215c916b20e5d2343
brahimjomni-zil <104820176+brahimjomni-zil@users.noreply.github.com>
2022-05-04 15:47:57 +0100

    Updating GetTxBlock to inherit from ResponsePayload
    Updating GetTxBlock to inherit from ResponsePayload

--- Assets/Scripts/Blockchain/Responses/GetTxBlockPayload.cs
+++ Assets/Scripts/Blockchain/Responses/GetTxBlockPayload.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GetTxBlockPayload : MonoBehaviour
+public class GetTxBlockPayload : ResponsePayload
 {
     public BlockBody body;
     public TxHeader header;
